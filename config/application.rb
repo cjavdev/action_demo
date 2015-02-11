d8d622d830a77b5034de3ec632d05e32a3fbd02e
CJ Avilla <cjavilla@gmail.com>
2015-02-11 14:39:13 -0800

    fix app

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module ActionDemo
     # config.i18n.default_locale = :de
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
-    config.middleware.use "ParamsFixer", "Load Time"
+    config.middleware.use "ParamsFixer"
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
